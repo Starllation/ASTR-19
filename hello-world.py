a209b2e782464c8b9a4217d639d7e3fbd67e013c
Charlotte <71458956+Starllation@users.noreply.github.com>
2026-03-31 13:07:50 -0700

    Added Hello-World program file
    The repository now is not empty!

---
+++ hello-world.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def PrintHelloWorld():
+    # This functions prints the words "Hello World!"
+    print('Hello World!')
+    
+def main():
+    PrintHelloWorld()
+    
+if __name__ == "__main__":
+    main()
