49c47324c3690928cbf07cd53bc1f1abdb15fdce
Charlotte <71458956+Starllation@users.noreply.github.com>
2026-04-08 17:51:13 -0700

    Session 1 Prompt

---
+++ session1.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  8 17:38:36 2026
+
+@author: charlotte
+"""
+
+def main():
+    print("My name is Charlotte Boren. My favorite movie is 'The Lion King.' My favorite food is grilled cheese sandwiches.")
+    
+    
+    
+    
+if __name__ == "__main__":
+    main()
