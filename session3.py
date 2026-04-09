6abeb37e03e1aa9beb57c41638129746f8cceae1
Charlotte <71458956+Starllation@users.noreply.github.com>
2026-04-08 18:20:53 -0700

    Session 3 Prompt

---
+++ session3.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  8 18:12:18 2026
+
+@author: charlotte
+"""
+
+def f(x):
+    return x**3 + 8
+
+def main():
+    x = f(9)
+    if x > 27:
+        print("YAY!")
+    
+    
+    
+if __name__ == "__main__":
+    main()
